9973185b1df60e7a96ece1281d1eb7134ef008e7
tamararom <trom@pmfst.hr>
2020-11-08 19:05:36 +0100

    Vjezbe_6 uz zad2 dovrsen

--- Vjezbe_6/Analyzer.cpp
+++ Vjezbe_6/Analyzer.cpp
@@ -60,15 +60,48 @@ void Analyzer::Loop()
 
 void Analyzer::PlotHistogram()
 {
-	TH1F *histo0, *histo1, *histo2, *histo3, *histo4;
+	TH1F *histo0, *histo1, *histo2, *histo3; //CANVAS 1,1
+	TH1F *histo4;
+	TH1F *histo02, *histo12, *histo22, *histo32; //CANVAS 1,2
+	TH1F *histo03, *histo13, *histo23, *histo33; //CANVAS 1,3
+	TH1F *histo04, *histo14, *histo24, *histo34; //CANVAS 1,4
 	
-	histo0=	new TH1F("pt1", "Transversal momenta of decayed particles", 250,0,140); //prvi broj je koliko ima binova?
+	//TRANSVERSAL MOMENTA
+	histo0=	new TH1F("LepPt1", "Transversal momentum", 50,0,140); //prvi broj je koliko ima binova?
 	//drugi je odakle krece, treci dokle ide
 	//prvi string " " je naslov u legendi, drugi je naslov histograma
-	histo1=	new TH1F("pt2", " ", 250,0,140);
-	histo2=	new TH1F("pt3", " ", 250,0,140);
-	histo3=	new TH1F("pt4", " ", 250,0,140);
-	histo4=new TH1F("4leptons", "mass reconstruction", 100, 90,140);
+	
+	//### SIRINA BINA JE (TRECI BROJ-DRUGI BROJ)/PRVI BROJ!!!!
+	//### ODNOSNO PRVI BROJ JE BROJ BINOVA, DRUGI I TRECI SU INTERVALI 
+	//### ZNACI ZA 50,0,140 IMAMO 50 BINOVA, I BIN SIZE JE 2.8 GEV
+	histo1=	new TH1F("LepPt2", "Transversal momentum",50,0,140);
+	histo2=	new TH1F("LepPt3", "Transversal momentum", 50,0,140);
+	histo3=	new TH1F("LepPt4", "Transversal momentum", 50,0,140);
+	
+	histo4=new TH1F("4leptons", "Mass reconstruction", 100, 90,140);
+	
+	//PSEUDORAPIDITY
+	histo02=new TH1F("LepEta1", "Pseudorapidity", 80,-4,4);
+	histo12=new TH1F("LepEta2", "Pseudorapidity", 80,-4,4);
+	histo22=new TH1F("LepEta3", "Pseudorapidity", 80,-4,4);
+	histo32=new TH1F("LepEta4", "Pseudorapidity", 80,-4,4);
+	//BIN SIZE JE 0.1 GEV
+	
+	//AZIMUTHAL ANGLE
+	histo03=new TH1F("LepPhi1", "Azimuthal angle", 80,-5.2,5.2);
+	histo13=new TH1F("LepPhi2", " ", 80,-5.2,5.2);
+	histo23=new TH1F("LepPhi3", " ", 80,-5.2,5.2);
+	histo33=new TH1F("LepPhi4", " ", 80,-5.2,5.2);
+	//BIN SIZE JE 0.13 GEV
+	
+	
+	//BDT 
+	histo04=new TH1F("LepBDT1", "BDT", 30,0,10);
+	histo14=new TH1F("LepBDT2", " ", 30,0,10);
+	histo24=new TH1F("LepBDT3", " ", 30,0,10);
+	histo34=new TH1F("LepBDT4", " ", 30,0,10);
+	//BIN SIZE JE 0.33 GEV
+	
 	
 	TLorentzVector *particle0;
 	TLorentzVector *particle1;
@@ -105,73 +138,153 @@ void Analyzer::PlotHistogram()
 	  
 	  //punjenje histograma
 	  
-	  /*histo0->Fill(LepPt->at(0)); //u zagrade cime ga zelim fillovat
-	  histo1->Fill(LepPt->at(1));
-	  histo2->Fill(LepPt->at(2));
-	  histo3->Fill(LepPt->at(3));*/
+	  w=(L*1000*xsec*overallEventWeight)/histogram->GetBinContent(40);
 	  
-	  particle0->SetPtEtaPhiM(LepPt->at(0),LepEta->at(0),LepPhi->at(0),0);
+	  //############ VJ6.ZAD2 ###########
+	  //TRANSVERSAL MOMENTA
+	  histo0->Fill(LepPt->at(0),w); //u zagrade cime ga zelim fillovat
+	  histo1->Fill(LepPt->at(1),w);
+	  histo2->Fill(LepPt->at(2),w);
+	  histo3->Fill(LepPt->at(3),w);
+	  
+	  //PSEUDORAPIDITY
+	  histo02->Fill(LepEta->at(0),w);
+	  histo12->Fill(LepEta->at(1),w);
+	  histo22->Fill(LepEta->at(2),w);
+	  histo32->Fill(LepEta->at(3),w);
+	  
+	  //AZIMUTHAL ANGLE
+	  histo03->Fill(LepPhi->at(0),w);
+	  histo13->Fill(LepPhi->at(1),w);
+	  histo23->Fill(LepPhi->at(2),w);
+	  histo33->Fill(LepPhi->at(3),w);
+	  
+	  //BDT
+	  histo04->Fill(LepBDT->at(0),w);
+	  histo14->Fill(LepBDT->at(1),w);
+	  histo24->Fill(LepBDT->at(2),w);
+	  histo34->Fill(LepBDT->at(3),w);
+	  
+	  /*particle0->SetPtEtaPhiM(LepPt->at(0),LepEta->at(0),LepPhi->at(0),0);
 	  particle1->SetPtEtaPhiM(LepPt->at(1),LepEta->at(1),LepPhi->at(1),0);
 	  particle2->SetPtEtaPhiM(LepPt->at(2),LepEta->at(2),LepPhi->at(2),0);
 	  particle3->SetPtEtaPhiM(LepPt->at(3),LepEta->at(3),LepPhi->at(3),0);
 	  
 	  *Z1=*particle0+*particle1;
-	  *Z2=*particle2+*particle3;
+	  *Z2=*particle2+*particle3;*/
 	  
 	  //cout << histogram->GetBinContent(40) << endl;
 	  
 	  //*1000 JER JE JEDNO U PIKO A DRUGO U FEMTOBARNIMA NA -1
-	  w=(L*1000*xsec*overallEventWeight)/histogram->GetBinContent(40);
-	  *Higgs=*Z1+*Z2;
-	  histo4->Fill(Higgs->M(),w);
+	  //w=(L*1000*xsec*overallEventWeight)/histogram->GetBinContent(40);
+	 /* *Higgs=*Z1+*Z2;
+	  histo4->Fill(Higgs->M(),w);*/
 	  
    }
    
 	//stvaram platno za crtanje histograma
 	
-	/*TCanvas *canvas1;
-	canvas1=new TCanvas("canvas1", "canvas1", 800, 1000); //dimenzija pixela x i y
+	TCanvas *canvas1;
+	canvas1=new TCanvas("canvas1", "canvas1", 1600, 900); //dimenzija pixela x i y
 	
 	//c1->cd(); ako zelim da ovaj bude u kojeg ce crtat jer Draw po defaultu crta u zadnje stvoreni Canvas
-	//canvas1->Divide(2,2);
-	//canvas1->cd(1);
-	
-	histo0->Draw(); //stvaram histogram
-	histo1->Draw("same");
-	histo2->Draw("same");
-	histo3->Draw("same");
-	
+	canvas1->Divide(2,2);
+	canvas1->cd(1);
+	histo2->Draw("HISTO");
+	histo3->Draw("HISTO same");
+	histo0->Draw("HISTO same"); //stvaram histogram
+	histo1->Draw("HISTO same");
 	//postavljam naslove osi u histogramu
-	histo1->GetXaxis()->SetTitle("Transversal momentum [GeV/c]");
-	histo1->GetYaxis()->SetTitle("Events");
-	
-	
+	histo2->GetXaxis()->SetTitle("p_{T} [GeV/c]");
+	histo2->GetYaxis()->SetTitle("Events/2.8GeV");
 	//mijenjam boje histograma
 	histo0->SetLineColor(kRed);
 	histo0->SetFillColor(kRed);
 	histo1->SetLineColor(kBlue);
 	histo2->SetLineColor(kYellow);
 	histo3->SetLineColor(kGreen);
-	
 	//postavljanje legende
 	gStyle->SetOptStat(0000);
 	TLegend *legend = new TLegend(.75,.75,1.0,.95);  //x1,y1,x2,y2 are the coordinates of the Legend
-	legend->SetHeader("Results of my simulation", "C"); //C for centering
+	//legend->SetHeader(" ", "C"); //C for centering
     legend->AddEntry(histo0,"First lepton");
     legend->AddEntry(histo1,"Second lepton");
 	legend->AddEntry(histo2,"Third lepton");
 	legend->AddEntry(histo3,"Fourth lepton");
-	
     legend->Draw();
+	
+	canvas1->cd(2);
+	histo32->Draw("HISTO");
+	histo12->Draw("HISTO same");
+	histo02->Draw("HISTO same");
+	histo22->Draw("HISTO same");
+	histo32->GetXaxis()->SetTitle("Pseudorapidity");
+	histo32->GetYaxis()->SetTitle("Events/0.1Gev");
+	histo02->SetLineColor(kRed);
+	histo02->SetFillColor(kRed);
+	histo12->SetLineColor(kBlue);
+	histo22->SetLineColor(kYellow);
+	histo32->SetLineColor(kGreen);
+	gStyle->SetOptStat(0000);
+	TLegend *legend2 = new TLegend(.75,.75,1.0,.95);  //x1,y1,x2,y2 are the coordinates of the Legend
+	//legend2->SetHeader(" ", "C"); //C for centering
+    legend2->AddEntry(histo02,"First lepton");
+    legend2->AddEntry(histo12,"Second lepton");
+	legend2->AddEntry(histo22,"Third lepton");
+	legend2->AddEntry(histo32,"Fourth lepton");
+    legend2->Draw();
+	
+	
+	canvas1->cd(3);
+	histo03->Draw("HISTO");
+	histo23->Draw("HISTO same");
+	histo33->Draw("HISTO same");
+	histo13->Draw("HISTO same");
+	histo03->GetXaxis()->SetTitle("Azimuthal angle [rad] ");
+	histo03->GetYaxis()->SetTitle("Events/0.13GeV");
+	histo03->SetLineColor(kRed);
+	histo03->SetFillColor(kRed);
+	histo13->SetLineColor(kBlue);
+	histo23->SetLineColor(kYellow);
+	histo33->SetLineColor(kGreen);
+	gStyle->SetOptStat(0000);
+	TLegend *legend3 = new TLegend(.75,.75,1.0,.95);  //x1,y1,x2,y2 are the coordinates of the Legend
+	//legend3->SetHeader(" ", "C"); //C for centering
+    legend3->AddEntry(histo03,"First lepton");
+    legend3->AddEntry(histo13,"Second lepton");
+	legend3->AddEntry(histo23,"Third lepton");
+	legend3->AddEntry(histo33,"Fourth lepton");
+    legend3->Draw();
+	
+	canvas1->cd(4);
+	histo04->Draw("HISTO");
+	histo24->Draw("HISTO same");
+	histo34->Draw("HISTO same");
+	histo14->Draw("HISTO same");
+	histo04->GetXaxis()->SetTitle("BDT");
+	histo04->GetYaxis()->SetTitle("Events/0.33GeV");
+	histo04->SetLineColor(kRed);
+	histo04->SetFillColor(kRed);
+	histo14->SetLineColor(kBlue);
+	histo24->SetLineColor(kYellow);
+	histo34->SetLineColor(kGreen);
+	gStyle->SetOptStat(0000);
+	TLegend *legend4 = new TLegend(.75,.75,1.0,.95);  //x1,y1,x2,y2 are the coordinates of the Legend
+	//legend4->SetHeader(" ", "C"); //C for centering
+    legend4->AddEntry(histo04,"First lepton");
+    legend4->AddEntry(histo14,"Second lepton");
+	legend4->AddEntry(histo24,"Third lepton");
+	legend4->AddEntry(histo34,"Fourth lepton");
+    legend4->Draw();
 	
 	
 	//spremam histogram u razlicite formate
-	canvas1->SaveAs("VJ6.pdf");
-	canvas1->SaveAs("VJ6.png");
-	canvas1->SaveAs("VJ6.root");*/
+	canvas1->SaveAs("VJ6_zd2.pdf");
+	canvas1->SaveAs("VJ6_zd2.png");
+	canvas1->SaveAs("VJ6_zd2.root");
 	
 	
-	TCanvas *canvas2;
+	/*TCanvas *canvas2;
 	canvas2=new TCanvas("canvas2", "canvas2", 800,1000);
 	histo4->Draw("HISTO");
 	
@@ -186,7 +299,6 @@ void Analyzer::PlotHistogram()
     legend->AddEntry(histo4,"gluon-gluon fusion");
 	legend->Draw();
 	
-	
-	canvas2->SaveAs("VJ6_zd4.pdf");
+	canvas2->SaveAs("VJ6_zd4.pdf");*/
 }
 
